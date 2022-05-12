625afcddf669a9362115c2aff17b53f95f28bf1f
kelechi <kelechi.akoma@yahoo.com>
2022-05-12 14:33:55 +0100

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
-const res = require('express/lib/response')
 const app = express()
+const dotenv = require('dotenv')
+const { default: mongoose } = require('mongoose')
+
+dotenv.config()
+
+// db COnnection
+mongoose.connect(process.env.DB_CONNECT,()=>{console.log('connected')})
 
 app.listen(3000,()=>{
     console.log('WOrking')
