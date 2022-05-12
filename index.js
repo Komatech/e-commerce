d1e4605331d3f9f725410d7193500b67aae75df8
kelechi <kelechi.akoma@yahoo.com>
2022-05-12 15:47:38 +0100

    fourth commit

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const { default: mongoose } = require('mongoose')
+const route = require('./routes/index')
 
 dotenv.config()
 
 // db COnnection
 mongoose.connect(process.env.DB_CONNECT,()=>{console.log('connected')})
 
+// app middleware
+app.use('/api',route)
+
+// port
 app.listen(3000,()=>{
     console.log('WOrking')
 })
